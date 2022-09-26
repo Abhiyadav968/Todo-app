11d9dfd08b5ff412742a9d2eccaa4765a332799e
abhiSuvaidyam <abhi.suvaidyam@gmail.com>
2022-09-26 15:59:56 +0530

    modofied indedx

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function table() {
   let table = `<table class="table">
     <thead>
       <tr>
-        <th scope="col">No</th>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Edit</th>
-        <th scope="col">Delete</th>
+        <th class="col-1">No</th>
+        <th class="col-3">Name</th>
+        <th class="col-4">Email</th>
+        <th class="col-2">Edit</th>
+        <th class="col-2">Delete</th>
       </tr>
     </thead>
     <tbody>`;
@@ -45,6 +45,11 @@ table();
 function save() {
   let name = document.getElementById("name");
   let email = document.getElementById("email");
+
+  if (name.value == 0) {
+    alert("Name and Email is Recqurid");
+    return
+  }
 
   let data = {
     Name: name.value,
@@ -72,15 +77,15 @@ function edit(index) {
 
   let editForm = `<div>
 <div class="form-group">
-  <label for="name">Update Name</label>
+  <label for="newName">Update Name</label>
   <input type="text" value="${details[index].Name}" class="form-control" id="newName" aria-describedby="emailHelp" placeholder="Update your Name">
 </div>
 <div class="form-group mt-3">
-  <label for="email">Update Email</label>
+  <label for="newEmail">Update Email</label>
   <input type="email" value="${details[index].Email}" class="form-control" id="newEmail" placeholder="Update your Email">
 </div>
 
-<button type="submit" class="btn btn-primary mt-3"  onclick="update()">Update</button>
+<button type="submit" class="btn btn-primary mt-3"  onclick="update(${index})">Update</button>
 </div> `;
 
   document.getElementById("form").innerHTML = editForm;
@@ -88,3 +93,16 @@ function edit(index) {
 
 };
 
+function update(index) {
+  let newName = document.getElementById("newName");
+  let newEmail = document.getElementById("newEmail");
+
+  details[index] = {
+    Name: newName.value,
+    Email: newEmail.value,
+  };
+  table();
+  document.getElementById("form").innerHTML = form;
+  console.log('update work')
+  console.log(details)
+}
